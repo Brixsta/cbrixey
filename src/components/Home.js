1dac396a45ff378dd59768ba28f226d1b7333497
Charles Brixey <charlesbrixey@Charless-MacBook-Pro.local>
2021-07-16 20:39:59 -0500

    test

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+    <div className={"homeWrapper"}>
+        <section className={"homeCanvas"}>
+            <h1 className={'homeTitle'}>Charles Brixey</h1>
+            <p className={'selfDescription'}>
+            <span>Professional Experience</span>
+             I've worked with Javascript, Git, CSS/HTML, NodeJS, ExpressJS React, Postgres, and Jquery.
+             Check out the Projects tab to see me utilize these technologies.
+            </p>
+            <div className={"selfPortrait"}>
+                <img height="100%" width="100%" src="https://objective-meitner-20183b.netlify.app/profilepic.jpeg"></img>
+            </div>
+        </section>
+    </div>
+    );
+}
+
+export default Home;
---
+++ src/components/Projects.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const Projects = () => {
+    return (
+        <div className={"projectsWrapper"}>
+            <section className={"projectsCanvas"}>
+                <h1 className={"projectsTitle"}>Projects</h1>
+                <div className={"projectsBox"} onClick={()=> window.open("https://brave-disgust.surge.sh/", "_blank")}>
+                    <div className={"projectsBoxImg"}>
+                        <img src="splatter.jpg" height="90%" width ="90%"></img>
+                    </div>
+                    <div className={"projectsBoxTitle"}>Splatter</div>
+                </div>
+                <div className={"projectsBox"} onClick={()=> window.open("https://lovely-baseball.surge.sh/", "_blank")}>
+                    <div className={"projectsBoxImg"}>
+                    <img src="catsonclick.jpg" height="90%" width ="90%"></img>
+                    </div>
+                    <div className={"projectsBoxTitle"}>Cats On Click</div>
+                </div>
+                <div className={"projectsBox"} onClick={()=> window.open("https://guiltless-wheel.surge.sh/", "_blank")}>
+                    <div className={"projectsBoxImg"}>
+                    <img src="tictactoe.jpg" height="90%" width ="90%"></img>
+                    </div>
+                    <div className={"projectsBoxTitle"}>Tic Tac Toe</div>
+                </div>
+                <div className={"projectsBox"} onClick={()=> window.open("https://hidden-plateau-56299.herokuapp.com/", "_blank")}>
+                    <div className={"projectsBoxImg"}>
+                    <img src="deadceleb.jpg" height="90%" width ="90%"></img>
+                    </div>
+                    <div className={"projectsBoxTitle"}>Dead Celeb Database</div>
+                </div>
+                <div className={"projectsBox"} onClick={()=> window.open("http://mcsp-ebay.ml", "_blank")}>
+                    <div className={"projectsBoxImg"}>
+                    <img src="fce.jpg" height="90%" width ="90%"></img>
+                    </div>
+                    <div className={"projectsBoxTitle"}>Ebay Clone</div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Projects;
